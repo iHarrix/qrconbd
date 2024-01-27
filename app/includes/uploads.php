0b8ba93aab137d85cfdc8106e687ddbae12918f3
Harrinson Moncada Jheremy <jheremy_sagitario_16@hotmail.com>
2024-01-27 00:20:16 -0500

    first commit

---
+++ app/includes/uploads.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+    /* Main */
+    'logo_light' => [
+        'whitelisted_file_extensions' => ['jpg', 'jpeg', 'png', 'svg', 'gif', 'webp'],
+        'path' => 'main/',
+    ],
+    'logo_dark' => [
+        'whitelisted_file_extensions' => ['jpg', 'jpeg', 'png', 'svg', 'gif', 'webp'],
+        'path' => 'main/',
+    ],
+    'logo_email' => [
+        'whitelisted_file_extensions' => ['jpg', 'jpeg', 'png', 'gif'],
+        'path' => 'main/',
+    ],
+    'favicon' => [
+        'whitelisted_file_extensions' => ['jpg', 'jpeg', 'png', 'ico', 'svg', 'gif', 'webp'],
+        'path' => 'main/',
+    ],
+    'opengraph' => [
+        'whitelisted_file_extensions' => ['jpg', 'jpeg', 'png', 'svg', 'gif', 'webp'],
+        'path' => 'main/',
+    ],
+
+    /* Blog featured images */
+    'blog' => [
+        'whitelisted_file_extensions' => ['jpg', 'jpeg', 'png', 'svg', 'gif', 'webp'],
+        'path' => 'blog/',
+    ],
+
+    /* Payment proofs for offline payments */
+    'offline_payment_proofs' => [
+        'whitelisted_file_extensions' => ['jpg', 'jpeg', 'png', 'pdf'],
+        'path' => 'offline_payment_proofs/',
+    ],
+
+    /* QR codes */
+    'qr_codes' => [
+        'path' => 'qr_codes/logo/'
+    ],
+
+    'qr_codes/logo' => [
+        'whitelisted_file_extensions' => ['jpg', 'jpeg', 'png', 'gif'],
+        'path' => 'qr_codes/logo/'
+    ],
+];
---
+++ app/models/QrCode.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * @copyright Copyright (c) 2021 AltumCode (https://altumcode.com/)
+ *
+ * This software is exclusively sold through https://altumcode.com/ by the AltumCode author.
+ * Downloading this product from any other sources and running it without a proper license is illegal,
+ *  except the official ones linked from https://altumcode.com/.
+ */
+
+namespace Altum\Models;
+
+use Altum\Uploads;
+
+class QrCode extends Model {
+
+    public function delete($qr_code_id) {
+
+        if(!$qr_code = db()->where('qr_code_id', $qr_code_id)->getOne('qr_codes', ['qr_code_id', 'qr_code', 'qr_code_logo'])) {
+            return;
+        }
+
+        foreach(['qr_code', 'qr_code_logo'] as $image_key) {
+            Uploads::delete_uploaded_file($qr_code->{$image_key}, 'qr_codes/logo');
+        }
+
+        /* Delete from database */
+        db()->where('qr_code_id', $qr_code_id)->delete('qr_codes');
+    }
+}
